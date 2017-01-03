22fa6c95bee6290ad8758dd5a7c5bf66f7c01b9a
JeremyPP <jimypougnet@gmail.com>
2017-01-03 14:11:00 +0100

    BUTTONS PAGE - added more stuff on the page, commented code and started to add content.

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,17 @@
+
+// Initialize code highlighting plugin.
+
+hljs.configure({
+	tabReplace: '    ' // 4 spaces
+})
+hljs.initHighlightingOnLoad();
+	
+
 $(document).ready(function() {
 
+
+	// nav dropdowns (left navbar).
+	
 	$('.leftContainerSection nav dl dt').on('click', function() {
 		$(this).next().toggle('fast');
 	});
@@ -8,7 +20,7 @@ $(document).ready(function() {
 	$('.leftContainerSection nav dl dt.active').next().show();
 	
 	
-	
+	// Add line number on code preview.
 	
 	(function() {
 	var pre = document.getElementsByTagName('pre'),
@@ -24,14 +36,40 @@ $(document).ready(function() {
 	})();
 	
 	
+	// Toggle light/dark theme for preview result/
 	
-	
-	$("#changeTheme_result").on( "click", function(){
+	$(".changeTheme_result").on( "click", function(){
 		if ( $(this).is(':checked') ){
 			$(".elementSnippetContainer .resultItemPreview").css("background", "#6e6e6e");
 		}else{
 			$(".elementSnippetContainer .resultItemPreview").css("background", "initial");
 		}
 	});
+	
+	
+	// Externalize as jahia_components.js
+	
+	var addRippleEffect = function (e) {
+		var target = e.target;
+		if (target.tagName.toLowerCase() !== 'button') return false;
+		var rect = target.getBoundingClientRect();
+		var ripple = target.querySelector('.ripple');
+		if (!ripple) {
+			ripple = document.createElement('span');
+			ripple.className = 'ripple';
+			ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+			target.appendChild(ripple);
+		}
+		ripple.classList.remove('show');
+		var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+		var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+		ripple.style.top = top + 'px';
+		ripple.style.left = left + 'px';
+		ripple.classList.add('show');
+		return false;
+	}
+
+	document.addEventListener('click', addRippleEffect, false);
+
 
 });
